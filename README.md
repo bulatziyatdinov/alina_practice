6fad84df32d9afb033057b2ceb6dfbaa5c82494a
ziyatdinov <bulat.ziyatdinov.05@mail.ru>
2026-07-07 17:21:16 +0300

    config change

---
+++ README.md
@@ -0,0 +1,19 @@
+Запустить Олламу
+```bash
+ollama serve
+```
+
+В другой консоли:
+
+```bash
+git clone https://github.com/bulatziyatdinov/alina_practice.git
+```
+
+```bash
+pip install uv
+```
+
+
+```bash
+uv run main.py
+```
--- src/config.py
+++ src/config.py
@@ -1,24 +1,20 @@
 import os
 
-# Настройки путей проекта
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 DATA_DIR = os.path.join(BASE_DIR, "data")
 INDEX_DIR = os.path.join(BASE_DIR, "index")
 DB_PATH = os.path.join(BASE_DIR, "chat_history.db")
 
-# Настройки моделей Ollama
 EMBEDDING_MODEL = "nomic-embed-text"
-LLM_MODEL = "qwen2.5:1.5b"
-LLM_MODEL = "qwen3.5:2b-q4_K_M"
+LLM_MODEL = "qwen2.5:7b"
 OLLAMA_BASE_URL = "http://localhost:11434"
 REASONING = False
 CONTEXT_LENGTH = 4096
 CHAT_MESSAGES_LIMIT = 10
 
-# Настройки разбиения текста (чанкинга)
 CHUNK_SIZE = 1000
 CHUNK_OVERLAP = 200
 
 # Настройки гибридного поиска (количество возвращаемых документов)
-FAISS_K = 3
-BM25_K = 3
+FAISS_K = 5
+BM25_K = 5
